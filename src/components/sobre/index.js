115c98346fedc48e627be713508cc07f7d90dd99
hakawa <hakawa.alves@gmail.com>
2020-05-29 20:55:57 -0300

    Altera o texto da aba sobre

--- src/components/sobre/index.js
+++ src/components/sobre/index.js
@@ -19,19 +19,30 @@ const Sobre = () => {
 			<Row>
 				<Col xs={12} md={6} lg={6}>
 					<span className="texto-sobre-container">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin malesuada eros vel suscipit vulputate.
-				Nam elit ante, pellentesque eget pellentesque ac, venenatis in velit. Donec eget nulla dapibus, ullamcorper sapien dapibus, mattis quam. 
-				Donec eget neque sed massa faucibus laoreet. Fusce hendrerit enim a blandit pulvinar. Proin egestas id risus in scelerisque. 
-				Quisque velit justo, lobortis et turpis at, condimentum cursus lectus. Integer eu fringilla tellus, in scelerisque purus. 
-				Quisque quam nisi, aliquet eu congue et, scelerisque id massa. Maecenas convallis tortor elit, 
-				vel vulputate augue laoreet eget. Maecenas euismod neque nec elit varius, maximus condimentum orci consectetur.
-				Vestibulum vehicula sagittis odio. Quisque at felis arcu. Nam sit amet ante vel ante porta pharetra. 
-				Quisque semper euismod enim at commodo. Praesent sit amet vestibulum enim. Phasellus in quam dolor. 
-				Sed sed leo dapibus, mollis erat at, tincidunt sem. Nullam rutrum turpis vel erat imperdiet lobortis.
-				Mauris nec massa sed sapien ultrices porttitor sit amet ac lectus. Fusce at tristique mauris.
+							A For Freelancers ou FoF para os mais íntimos nasceu com o intuito de inovar as negociações e gestões que são feitas entre freelancers e empresas, utilizando as melhores ferramentas do mercado para poder trazer uma experiência segura e agradável a todos nossos usuários.
+						<br/>
+						<br/>
+						<h2 className="center">Missão</h2>
+						<span>
+							É trazer a transformação digital alinhada a estratégia na contratação de um ou vários freelancers, utilizando nossas métricas que foram pensadas e alinhadas, de modo que traga mais resultado e livre nosso cliente de preocupações.
+							<ul>
+								<li>
+									Agilizar e Otimizar Processos;
+								</li>
+								<li>
+									Reduzir Custos e Complexidade;
+								</li>
+								<li>
+									Minimizar Falhas e Riscos;
+								</li>
+							</ul>
+						</span>
+						<br/>
+						<h2 className="center">Visão</h2>
+						<span>Poder mudar o cenário atual de contratação de freelancers, e poder usar essa mesma base para renovar a metodologia utilizada em outros tipos de contratações.</span>
 					</span>
 				</Col>
-				{!mobile && <Col xs={12} md={6} lg={6}><img className="img-sobre-container" src={sobre}/></Col>}
+				<Col xs={12} md={6} lg={6}><img className="img-sobre-container" src={sobre}/></Col>
 			</Row>
 		</div>
 	)
