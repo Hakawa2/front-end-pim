bced11ab260f16cab08633511de22b1bcdca58dc
hakawa <hakawa.alves@gmail.com>
2020-05-29 21:29:18 -0300

    Feedbacks

--- src/mocks/clientes/index.js
+++ src/mocks/clientes/index.js
@@ -2,55 +2,49 @@ export const relatos = [
 	{
 		nome: "André Carlos",
 		idade: 19,
-		mensagem: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam urna nulla, tempor nec eros eu, pharetra viverra nunc. Praesent a consequat eros, quis interdum elit. ",
+		mensagem: "Atendimento ótimo, rápido e super confiável recomendo d++++++++. ",
 		sexo: "M"
 	},
 	{
 		nome: "John C. Wilson",
 		idade: 34,
-		mensagem: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam urna nulla, tempor nec eros eu, pharetra viverra nunc. Praesent a consequat eros, quis interdum elit. ",
+		mensagem: "Ajudou muito a minha empresa na gestão dos freelancers, algo que seria apenas um contrato se tornaram 10 graças a praticidade da plataforma.",
 		sexo: "M"
 	},
 	{
 		nome: "Tom L. Charles",
 		idade: 22,
-		mensagem: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam urna nulla, tempor nec eros eu, pharetra viverra nunc. Praesent a consequat eros, quis interdum elit. ",
+		mensagem: "Como freelancer posso dizer que a plataforma me ajudou demais.",
 		sexo: "M"
 	},
 	{
 		nome: "Jonathan R. Bynum",
 		idade: 37,
-		mensagem: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam urna nulla, tempor nec eros eu, pharetra viverra nunc. Praesent a consequat eros, quis interdum elit. ",
+		mensagem: "Nunca imaginei que seria tão fácil contratar um freelancers, graças a Fof perdi o medo e consegui contratar um para minha empresa.",
 		sexo: "M"
 	},
 	{
 		nome: "Sandra R. Zepeda",
 		idade: 39,
-		mensagem: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam urna nulla, tempor nec eros eu, pharetra viverra nunc. Praesent a consequat eros, quis interdum elit. ",
+		mensagem: "Ótimo atendimento!!",
 		sexo: "F"
 	},
 	{
 		nome: "Eartha R. Hale",
 		idade: 49,
-		mensagem: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam urna nulla, tempor nec eros eu, pharetra viverra nunc. Praesent a consequat eros, quis interdum elit. ",
+		mensagem: "Muito bom!",
 		sexo: "F"
 	},
 	{
 		nome: "Jeanne L. Winters",
 		idade: 62,
-		mensagem: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam urna nulla, tempor nec eros eu, pharetra viverra nunc. Praesent a consequat eros, quis interdum elit. ",
+		mensagem: "Excelente atendimento, produto de qualidade com o preço justo e entrega!! Parabéns pelos serviços!",
 		sexo: "F"
 	},
 	{
 		nome: "Linda M. Linton",
 		idade: 55,
-		mensagem: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam urna nulla, tempor nec eros eu, pharetra viverra nunc. Praesent a consequat eros, quis interdum elit. ",
-		sexo: "F"
-	},
-	{
-		nome: "Kimberly E. Hutchins",
-		idade: 23,
-		mensagem: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam urna nulla, tempor nec eros eu, pharetra viverra nunc. Praesent a consequat eros, quis interdum elit. ",
+		mensagem: "Ótimos profissionais.",
 		sexo: "F"
 	}
 ]
